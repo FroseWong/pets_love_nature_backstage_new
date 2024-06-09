a328babd9512b115efbed1c877121484c23c411f
FroseWong <tibafrose@gmail.com>
2024-06-09 16:40:51 +0800

    新增bootstrap和bootstrap使用範例

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1> -->
+    <div class="container">
+      <div class="container-sm">100% wide until small breakpoint</div>
+      <div class="container-md">100% wide until medium breakpoint</div>
+      <div class="container-lg">100% wide until large breakpoint</div>
+      <div class="container-xl">100% wide until extra large breakpoint</div>
+      <div class="container-xxl">100% wide until extra extra large breakpoint</div>
+    </div>
   </div>
 </template>
 
