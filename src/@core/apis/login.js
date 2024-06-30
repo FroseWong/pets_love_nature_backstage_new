0baed71baf7bc08851c6c64bb2d643a3019d7350
FroseWong <tibafrose@gmail.com>
2024-06-30 22:54:26 +0800

    登入 調整 try catch

--- src/@core/apis/login.js
+++ src/@core/apis/login.js
@@ -5,16 +5,18 @@ import router from '@/router'
 
 export const login = async (data) => {
   //   console.log('data', data)
-  const res = await useAxiosPost('/admin/signIn', data)
-  console.log('res?.data', res?.data)
-  console.log(res)
+  try {
+    const res = await useAxiosPost('/admin/signIn', data)
 
-  if (res?.status === 'success') {
-    // 成功登入
-    console.log('成功登入')
-    localStorage.setItem('token', res?.data?.token)
-    router.push({ path: '/products' })
-  } else {
+    if (res?.status === 'success') {
+      // 成功登入
+      console.log('成功登入')
+      localStorage.setItem('token', res?.data?.token)
+      router.push({ path: '/products' })
+    } else {
+      alert('登入失敗，請確認帳號密碼')
+    }
+  } catch (e) {
     alert('登入失敗，請確認帳號密碼')
   }
 }
